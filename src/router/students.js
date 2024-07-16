92ed605b5e57001e89df096862c3ea5151e3b523
Serhii Vasiliev <sepovi4@gmail.com>
2024-07-16 18:13:30 +0200

    risingProject

---
+++ src/router/students.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import {
+  getStudentsController,
+  getStudentByIdController,
+} from "../controllers/students";
+import { ctrlWrapper } from "../utils/ctrlWrapper";
+
+const router = Router();
+
+router.get("/students", ctrlWrapper(getStudentsController));
+
+router.get("/students/:studentId", ctrlWrapper(getStudentByIdController));
+
+export default router;
